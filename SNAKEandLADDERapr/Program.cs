38355ed1d148bc4f75107d09d7f14cb8c5b508e4
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-21 18:49:04 +0530

    checking conditions

--- SNAKEandLADDERapr/Program.cs
+++ SNAKEandLADDERapr/Program.cs
@@ -4,3 +4,26 @@ int playerposition = 0;
 Random random = new Random();
 int rollDie = random.Next(1, 7);
 Console.WriteLine(rollDie);
+int options = random.Next(0, 3);
+switch (options)
+{
+    case 0:
+        Console.WriteLine("NO_PLAY");
+        playerposition = 0;
+        break;
+    case 1:
+        Console.WriteLine("LADDER");
+        playerposition += rollDie;
+        break;
+    case 2:
+        Console.WriteLine("SNAKE");
+        playerposition -= rollDie;
+        if (playerposition < 0)
+        {
+            playerposition = 0;
+        }
+        break;
+    default:
+        break;
+}
+Console.WriteLine(playerposition);
